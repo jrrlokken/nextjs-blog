aaa5227195c84fe831473d8853925b6db6a3cc87
Joshua Lokken <joshualokken@pm.me>
2021-08-31 12:27:30 -0500

    Added Typescript

--- lib/posts.ts
+++ lib/posts.ts
@@ -18,7 +18,7 @@ export function getSortedPostsData() {
 
     return {
       id,
-      ...matterResult.data,
+      ...(matterResult.data as { date: string; title: string }),
     };
   });
 
@@ -59,6 +59,6 @@ export async function getPostData(id) {
   return {
     id,
     contentHtml,
-    ...matterResult.data,
+    ...(matterResult.data as { date: string; title: string }),
   };
 }
--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,6 +3,7 @@ import { getAllPostIds, getPostData } from "../../lib/posts";
 import Head from "next/head";
 import Date from "../../components/date";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from "next";
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id);
@@ -21,7 +22,15 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post({ postData }) {
+export default function Post({ 
+  postData 
+}: { 
+  postData: { 
+    title: string
+    date: string
+    contentHtml: string
+  }
+}) {
   return (
     <Layout>
       <Head>
